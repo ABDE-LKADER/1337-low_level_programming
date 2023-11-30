7a65cc7f3ecf068219960a5f768c15cc0e2c0f9f
Abdelkader Adouab <abadouab@e3r5p2.1337.ma>
2023-11-30 15:09:39 +0100

    UPDATE

--- LIBFT_42/ft_atoi.c
+++ LIBFT_42/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 19:22:38 by abadouab          #+#    #+#             */
-/*   Updated: 2023/11/28 19:11:54 by abadouab         ###   ########.fr       */
+/*   Updated: 2023/11/30 14:45:05 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ int	over_fix(int sign)
 
 int	ft_atoi(const char *str)
 {
-	int				i;
-	int				sign;
-	unsigned long	num;
+	int		i;
+	int		sign;
+	long	num;
+	long	check;
 
 	i = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
@@ -36,12 +37,13 @@ int	ft_atoi(const char *str)
 		i++;
 	}
 	num = 0;
+	check = 0;
 	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
 	{
-		num = num * 10 + str[i] - 48;
-		if (!(num < 9223372036854775807))
+		num = num * 10 + str[i++] - 48;
+		if (check > num)
 			return (over_fix(sign));
-		i++;
+		check = num;
 	}
 	return (num * sign);
 }
