bfed6a3c1168231276b0d677597a5eb15bb89ef0
ABDEL-KADER <abdelkader.adouab@edu.uca.ma>
2024-01-04 15:14:54 +0100

    CLIENT && SERVER

--- MINI_TALK/client.c
+++ MINI_TALK/client.c
@@ -6,16 +6,15 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 11:34:21 by abadouab          #+#    #+#             */
-/*   Updated: 2024/01/03 15:26:34 by abadouab         ###   ########.fr       */
+/*   Updated: 2024/01/03 17:38:07 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	signal_handler(int server_pid, char *message)
+void	signal_handler(int server_pid, char mes)
 {
-	kill(server_pid, SIGUSR1);
-	printf("%s\nDONE", message);
+	// 01001000
 }
 
 int	main(int ac, char **av)
@@ -26,15 +25,16 @@ int	main(int ac, char **av)
 	{
 		write(2, "\033[1;31mUsage: ", 14);
 		write(2, "\033[0m./client <server_pid> <message>\n", 36);
-		exit (EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	server_pid = atoi(av[1]);
 	if (server_pid <= 0)
 	{
 		write(2, "\033[1;31mError: \033[0mInvalid server PID.\n\033[0;33m", 45);
 		write(2, "-> Please provide a valid PID greater than 0.\n", 46);
-		exit (EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-	signal_handler(server_pid, av[2]);
+	while (*av[2])
+		signal_handler(server_pid, *av[2]++);
 	return (EXIT_SUCCESS);
 }
--- MINI_TALK/server.c
+++ MINI_TALK/server.c
@@ -6,32 +6,21 @@
 /*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 11:34:17 by abadouab          #+#    #+#             */
-/*   Updated: 2024/01/03 10:52:16 by abadouab         ###   ########.fr       */
+/*   Updated: 2024/01/03 18:24:17 by abadouab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void sig_handle1(){
-	printf("SIG ACTION 1\n");
-}
 
-void sig_handle2(){
-	printf("SIG ACTION 2\n");
-}
-
-void signal_h()
-{
-	signal(SIGUSR1, &sig_handle1);
-	signal(SIGUSR2, &sig_handle2);
-}
 
 int main()
 {
 	pid_t server_pid = getpid();
 
 	printf("The server PID: %d\n", server_pid);
-	signal_h();
+	signal(SIGUSR1, &sig_handle1);
+	signal(SIGUSR2, &sig_handle2);
 	while (1)
 		pause();
 	return EXIT_SUCCESS;
