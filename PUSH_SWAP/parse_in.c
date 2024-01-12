9b8b50449bcb937a365494a2ac5fbe7a96197c11
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-01-12 10:55:29 +0100

    FIX SEGV

---
+++ PUSH_SWAP/parse_in.c
@@ -0,0 +1,47 @@
+#include "push_swap.h"
+
+void	add_num(t_stack **stack, char *value)
+{
+	t_stack	*node;
+	t_stack *new;
+
+	if (!stack)
+		return ;
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		return ;
+	new->value = ft_atol(value);
+	new->next = NULL;
+	if (!(*stack))
+	{
+		*stack = new;
+		return ;
+	}
+	node = *stack;
+	while (node->next)
+		node = node->next;
+	node->next = new;
+}
+
+t_stack	*parce_in(int ac, char **av)
+{
+	t_stack	*stack = NULL;
+	char	**spn;
+	int		i;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (ft_strchr(av[i], ' '))
+		{
+			spn = ft_split(av[i], ' ');
+			while (*spn)
+				add_num(&stack, *spn++);
+		}
+		else
+			add_num(&stack, av[i]);
+		i++;
+	}
+	check_double(stack);
+	return (stack);
+}
--- PUSH_SWAP/push_swap.c
+++ PUSH_SWAP/push_swap.c
@@ -12,115 +12,19 @@
 
 #include "push_swap.h"
 
-static void	message_error(void)
-{
-	write(2, "Error\n", 6);
-	exit(EXIT_FAILURE);
-}
-
-static long	ft_atol(const char *str)
-{
-	int		i;
-	int		sign;
-	long	num;
-
-	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	sign = 1;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
-	}
-	num = 0;
-	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
-	{
-		num = num * 10 + str[i] - 48;
-		i++;
-		if (num * sign > INT_MAX || num * sign < INT_MIN)
-			message_error();
-	}
-	return (num * sign);
-}
-
-static void	check_in(int ac, char **av)
-{
-	int		i;
-	int		j;
-	char	*check;
-
-	i = 1;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			if ((!ft_strchr(" -+0123456789", av[i][j])))
-				message_error();
-			else if (ft_strchr("-+", av[i][j]) && !ft_isdigit(av[i][j + 1]))
-				message_error();
-			else if (ft_isdigit(av[i][j])
-				&& !ft_strchr(" 0123456789", av[i][j + 1]))
-				message_error();
-			j++;
-		}
-		check = ft_strtrim(av[i], " ");
-		if (!check[0])
-			message_error();
-		i++;
-	}
-}
-
-t_stack	*add_num(char *value)
-{
-	t_stack	*num;
-
-	num = malloc(sizeof(t_stack));
-	if (!num)
-		return (NULL);
-	num->value = ft_atol(value);
-	num->next = NULL;
-	return (num);
-}
-
-static	t_stack	*parce_in(int ac, char **av)
-{
-	t_stack	*stack;
-	char	**spn;
-	int		i;
-
-	i = 1;
-	while (i < ac)
-	{
-		if (ft_strchr(av[i], ' '))
-		{
-			spn = ft_split(av[i], ' ');
-			while (*spn)
-			{
-				stack = add_num(*spn++);
-				printf("%d\n", stack->value);
-				stack = stack->next;
-			}
-		}
-		else
-			stack = add_num(av[i]);
-		printf("%d\n", stack->value);
-		stack = stack->next;
-		i++;
-	}
-	stack->next = NULL;
-	return (stack);
-}
-
 int	main(int ac, char **av)
 {
-	t_stack	*stack_a;
+	t_stack	*stack_a = NULL;
 
 	if (ac == 1)
 		return (EXIT_SUCCESS);
+	(void)stack_a;
 	check_in(ac, av);
 	stack_a = parce_in(ac, av);
+	while (stack_a)
+	{
+		ft_printf("%d\n", stack_a->value);
+		stack_a = stack_a->next;
+	}
 	return (EXIT_SUCCESS);
 }
