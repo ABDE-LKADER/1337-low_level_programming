90530eb5ef755cac11d01043076ecf0e2f524a24
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2024-01-26 01:30:32 +0100

    UPDATE DIR CHANGED ...

---
+++ FT_PRINTF_42/mandatory/ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 19:31:09 by abadouab          #+#    #+#             */
+/*   Updated: 2024/01/01 17:59:49 by abadouab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "../MYLIB/mylib.h"
+# include <unistd.h>
+# include <stdarg.h>
+
+# define SPECIFIERS	"cspdiuxX%"
+
+int	print_char(char c);
+int	print_num(int num);
+int	print_string(char *str);
+int	print_address(void *ptr);
+int	print_unum(unsigned int num);
+int	print_hex(unsigned int num, char set);
+int	ft_printf(const char *format, ...);
+
+#endif
