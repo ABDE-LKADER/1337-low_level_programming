bc935d7725157daafa0860814e6e041d1fffd995
ABDEL-KADER <abdelkader.adouab@edu.uca.ma>
2024-03-31 14:09:20 +0000

    CHECK POINT <OK> ...

--- GET_NEXT_LINE_42/run.c
+++ GET_NEXT_LINE_42/run.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abadouab <abadouab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/31 14:09:14 by abadouab          #+#    #+#             */
+/*   Updated: 2024/03/31 14:09:16 by abadouab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line_bonus.h"
 #include <stdio.h>
 #include <fcntl.h>
@@ -5,15 +17,15 @@
 void fn(){system("leaks run");}
 int main (void)
 {
-    int fd = open("nl.txt", O_RDONLY);
-    char *line;
+	int fd = open("nl.txt", O_RDONLY);
+	char *line;
 
-    line = get_next_line(fd);
-    while (line)
-    {
-        printf("%s", line);
-        free(line);
-        line = get_next_line(fd);
-    }
-    atexit(fn);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	atexit(fn);
 }
